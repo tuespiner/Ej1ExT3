ae3c3c7a1737d7ec552dd09c38fe4445a9fa4058
tuespiner <juanspiner89@gmail.com>
2023-12-01 19:57:45 +0100

    Pero mi preferido es el chocolate

--- ProyectoExEj2/src/Turron.java
+++ ProyectoExEj2/src/Turron.java
@@ -3,7 +3,7 @@ public class Turron {
 
 	public static void main(String[] args) {
 		System.out.println("soy Juan López Carmona y me encantan los turrones");
-
+		System.out.println("Pero mi preferido es el chocolate");
 	}
 
 }
